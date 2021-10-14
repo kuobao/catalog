7034bb38aa84cfc473522c7232b92d886c4e46bd
Kuo Bao <alexkuobao@gmail.com>
2021-10-14 10:01:58 -0400

    Change RDS-instance

--- middleware/context.py
+++ middleware/context.py
@@ -11,28 +11,11 @@ def get_db_info():
     :return: A dictionary with connect info for MySQL
     """
 
-    db_host = os.environ.get("DBHOST", None)
-    db_user = os.environ.get("DBUSER", None)
-    db_password = os.environ.get("DBPASSWORD", None)
-    if db_host is not None:
-        db_info = {
-            "host": db_host,
-            "user": db_user,
-            "password": db_password,
-            "cursorclass": pymysql.cursors.DictCursor
-        }
-    else:
-        db_info = {
-            "host": "e6156-rds.clboe01dd0s5.us-east-2.rds.amazonaws.com",
-            "user": "root",
-            "password": "rootroot",
-            "cursorclass": pymysql.cursors.DictCursor
-        }
+    db_info = {
+        "host": "e6156-rds.clboe01dd0s5.us-east-2.rds.amazonaws.com",
+        "user": "root",
+        "password": "rootroot",
+        "cursorclass": pymysql.cursors.DictCursor
+    }
 
-    # db_info = {
-    #     "host": "catalogdb.clboe01dd0s5.us-east-2.rds.amazonaws.com",
-    #     "user": "admin",
-    #     "password": "Himalayas2021",
-    #     "cursorclass": pymysql.cursors.DictCursor
-    # }
     return db_info
