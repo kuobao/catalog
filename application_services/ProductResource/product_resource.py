7e316df804a7418b7bc0f4479e63c423d158415b
Kuo Bao <alexkuobao@gmail.com>
2021-10-14 16:17:43 -0400

    Add missing functions

--- application_services/ProductResource/product_resource.py
+++ application_services/ProductResource/product_resource.py
@@ -16,3 +16,24 @@ class ProductResource(BaseRDBApplicationResource):
         res = ProductResource.get_by_template({"id": pid})
         return res
 
+    @classmethod
+    def create(cls, data):
+        new_product = dict()
+        new_product['name'] = data['name']
+        new_product['price'] = data['price']
+        new_product['description'] = data['description']
+        new_product['category'] = data['category']
+        new_product['quantity'] = data['quantity']
+        new_product['image_url'] = data['image_url']
+        new_product['availability'] = data['availability']
+        res = RDBService.create("catalog", "product", new_product)
+        return res
+
+    @classmethod
+    def delete(cls, template):
+        res = RDBService.delete("catalog", "product", template)
+        return res
+
+    @classmethod
+    def update(cls, template, row):
+        res = RDBService.update("catalog", "product", template, row)
